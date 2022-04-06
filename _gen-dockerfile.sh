02c3bca2c5a1557de87f24130e7654c49cc692f1
Anucha Nualsi <ana.cpe9@gmail.com>
2022-04-06 21:58:13 +0700

    :loud_sound: print image and tag base for base image

--- _gen-dockerfile.sh
+++ _gen-dockerfile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo $REGISTRY_IMAGE:IMAGE_VERSION
+
 sed "s/%%IMAGE_VERSION%%/$IMAGE_VERSION/g" Dockerfile.template > dist/Dockerfile
 sed "s/%%REGISTRY_IMAGE%%:%%IMAGE_VERSION%%/${REGISTRY_IMAGE//\//\\/}:$IMAGE_VERSION/g" Dockerfile.builder.template > dist/Dockerfile.builder
 # https://stackoverflow.com/questions/584894/environment-variable-substitution-in-sed
